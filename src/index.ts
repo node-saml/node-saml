01b3254eb6f46ac65ee275b06ab89de27132b15a
Chris Barth <chrisjbarth@hotmail.com>
2021-06-23 14:36:45 -0400

    Add more exports (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type { CacheItem, CacheProvider } from "./inmemory-cache-provider";
 import { SAML } from "./saml";
+import { MandatorySamlOptions, Profile, SamlConfig, SamlOptions } from "./types";
 
-export { SAML, CacheItem, CacheProvider };
+export { SAML, CacheItem, CacheProvider, SamlOptions, MandatorySamlOptions, Profile, SamlConfig };
