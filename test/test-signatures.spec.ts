34b625ddbe556b20186fa1445257b88c5fee289f
Chris Barth <chrisjbarth@hotmail.com>
2022-07-20 15:29:18 -0400

    lint

--- test/test-signatures.spec.ts
+++ test/test-signatures.spec.ts
@@ -2,7 +2,7 @@ import { SAML } from "../src";
 import * as fs from "fs";
 import * as sinon from "sinon";
 import { SamlConfig } from "../src/types";
-import * as xml from '../src/xml';
+import * as xml from "../src/xml";
 import * as assert from "assert";
 import { expect } from "chai";
 
