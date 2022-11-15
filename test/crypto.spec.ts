60d36bc60223cc76aca906e04f8416f2b62724fc
Markku Roponen <roponen.markku@gmail.com>
2022-11-15 09:37:07 +0200

    Added public key tests into PEM handling.

--- test/crypto.spec.ts
+++ test/crypto.spec.ts
@@ -9,10 +9,16 @@ import {
   normalizeBase64Data,
   normalizePemFile,
 } from "../src/crypto";
-import { TEST_CERT_SINGLELINE, TEST_CERT_MULTILINE } from "./types";
+import {
+  TEST_CERT_SINGLELINE,
+  TEST_CERT_MULTILINE,
+  TEST_PUBLIC_KEY_SINGLELINE,
+  TEST_PUBLIC_KEY_MULTILINE,
+} from "./types";
 
 describe("crypto.ts", function () {
   const expectedCert = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----\n`;
+  const expectedPublicKey = `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_MULTILINE}\n-----END PUBLIC KEY-----\n`;
 
   describe("normalizeBase64Data", function () {
     it("normalizes singleline base64 data properly", function () {
@@ -37,6 +43,18 @@ describe("crypto.ts", function () {
       const certificate = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----`;
       const normalizedPem = normalizePemFile(certificate);
       expect(normalizedPem).to.equal(expectedCert);
+    });
+
+    it("normalizes public key PEM which has base64 data formatted into singleline", function () {
+      const publicKey = `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_SINGLELINE}\n-----END PUBLIC KEY-----`;
+      const normalizedPem = normalizePemFile(publicKey);
+      expect(normalizedPem).to.equal(expectedPublicKey);
+    });
+
+    it("normalizes public key PEM which has base64 data formatted into multiline", function () {
+      const publicKey = `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_MULTILINE}\n-----END PUBLIC KEY-----`;
+      const normalizedPem = normalizePemFile(publicKey);
+      expect(normalizedPem).to.equal(expectedPublicKey);
     });
   });
 
@@ -78,44 +96,88 @@ describe("crypto.ts", function () {
   });
 
   describe("keyInfoToPem", function () {
-    it("should return certificate in PEM format for multiline certificate", function () {
-      const certificate = keyInfoToPem(TEST_CERT_MULTILINE);
-      expect(certificate).to.equal(expectedCert);
+    describe("when key info is provided in PEM format", function () {
+      it("should return certificate in PEM format for multiline certificate", function () {
+        const certificate = keyInfoToPem(
+          `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----`
+        );
+        expect(certificate).to.equal(expectedCert);
+      });
+
+      it("should return certificate in PEM format for singleline certificate", function () {
+        const certificate = keyInfoToPem(
+          `-----BEGIN CERTIFICATE-----\n${TEST_CERT_SINGLELINE}\n-----END CERTIFICATE-----`
+        );
+        expect(certificate).to.equal(expectedCert);
+      });
+
+      it("should return public key in PEM format for multiline pubic key", function () {
+        const publicKey = keyInfoToPem(
+          `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_MULTILINE}\n-----END PUBLIC KEY-----`,
+          "PUBLIC KEY"
+        );
+        expect(publicKey).to.equal(expectedPublicKey);
+      });
+
+      it("should return public key in PEM format for singleline public", function () {
+        const publicKey = keyInfoToPem(
+          `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_SINGLELINE}\n-----END PUBLIC KEY-----`,
+          "PUBLIC KEY"
+        );
+        expect(publicKey).to.equal(expectedPublicKey);
+      });
     });
 
-    it("should return certificate in PEM format for singleline certificate", function () {
-      const certificate = keyInfoToPem(TEST_CERT_SINGLELINE);
-      expect(certificate).to.equal(expectedCert);
-    });
+    describe("when key info is provided in Base64 format", function () {
+      it("should return certificate in PEM format for multiline Base64 certificate", function () {
+        const certificate = keyInfoToPem(TEST_CERT_MULTILINE);
+        expect(certificate).to.equal(expectedCert);
+      });
 
-    it("should return certificate in PEM format for multiline certificate that already has PEM header and footer label", function () {
-      const certificate = keyInfoToPem(
-        `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----`
-      );
-      expect(certificate).to.equal(expectedCert);
-    });
+      it("should return certificate in PEM format for singleline Base64 certificate", function () {
+        const certificate = keyInfoToPem(TEST_CERT_SINGLELINE);
+        expect(certificate).to.equal(expectedCert);
+      });
 
-    it("should return certificate in PEM format for singleline certificate that already has PEM header and footer label", function () {
-      const certificate = keyInfoToPem(
-        `-----BEGIN CERTIFICATE-----\n${TEST_CERT_SINGLELINE}\n-----END CERTIFICATE-----`
-      );
-      expect(certificate).to.equal(expectedCert);
+      it("should return public key in PEM format for multiline Base64 public key", function () {
+        const publicKey = keyInfoToPem(TEST_PUBLIC_KEY_MULTILINE, "PUBLIC KEY");
+        expect(publicKey).to.equal(expectedPublicKey);
+      });
+
+      it("should return public key in PEM format for singleline Base64 public key", function () {
+        const publicKey = keyInfoToPem(TEST_PUBLIC_KEY_SINGLELINE, "PUBLIC KEY");
+        expect(publicKey).to.equal(expectedPublicKey);
+      });
     });
   });
 
   describe("stripPemHeaderAndFooter", function () {
     it("removes PEM header and footer from singleline certificate", function () {
-      const cert = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_SINGLELINE}\n-----END CERTIFICATE-----`;
-      const plainBase64Data = stripPemHeaderAndFooter(cert);
+      const certificate = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_SINGLELINE}\n-----END CERTIFICATE-----`;
+      const plainBase64Data = stripPemHeaderAndFooter(certificate);
 
       expect(plainBase64Data.trimEnd()).to.equal(TEST_CERT_SINGLELINE);
     });
 
     it("removes PEM header and footer from multiline certificate", function () {
-      const cert = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----`;
-      const plainBase64Data = stripPemHeaderAndFooter(cert);
+      const certificate = `-----BEGIN CERTIFICATE-----\n${TEST_CERT_MULTILINE}\n-----END CERTIFICATE-----`;
+      const plainBase64Data = stripPemHeaderAndFooter(certificate);
 
       expect(plainBase64Data.trimEnd()).to.equal(TEST_CERT_MULTILINE);
+    });
+
+    it("removes PEM header and footer from singleline public key", function () {
+      const publicKey = `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_SINGLELINE}\n-----END PUBLIC KEY-----`;
+      const plainBase64Data = stripPemHeaderAndFooter(publicKey);
+
+      expect(plainBase64Data.trimEnd()).to.equal(TEST_PUBLIC_KEY_SINGLELINE);
+    });
+
+    it("removes PEM header and footer from multiline public key", function () {
+      const publicKey = `-----BEGIN PUBLIC KEY-----\n${TEST_PUBLIC_KEY_MULTILINE}\n-----END PUBLIC KEY-----`;
+      const plainBase64Data = stripPemHeaderAndFooter(publicKey);
+
+      expect(plainBase64Data.trimEnd()).to.equal(TEST_PUBLIC_KEY_MULTILINE);
     });
   });
 });
