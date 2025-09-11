668c502ce3266163ea29989763546f73bfb20d75
Chris Barth <chrisjbarth@hotmail.com>
2025-09-11 12:20:38 -0500

    Use `util.debuglog()` instead of `console.log()`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,7 +35,7 @@ export default tseslint.config([
       "@typescript-eslint": eslintPluginTypeScript,
     },
     rules: {
-      "no-console": ["error", { allow: ["debug", "warn", "error"] }],
+      "no-console": ["error"],
       "@typescript-eslint/no-non-null-assertion": "error",
       "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/no-invalid-void-type": "off",
